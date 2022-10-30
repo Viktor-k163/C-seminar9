c9639c12deb4a2d0c5883ebdc631d1ffa6c2b002
Viktor <korolev-viktor92@mail.ru>
2022-10-30 15:40:10 +0400

    вторая задача готова

---
+++ Second/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Задайте значение N: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Задайте значение M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine(RecursSum(n, m));
+
+int RecursSum(int n, int m)
+{
+  if (n == m)
+  {
+  return n;
+  }
+return m + RecursSum(n, m - 1);
+}
